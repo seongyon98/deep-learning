586593745bd36e6e07aa4d5953c208d3c9534a36
승연 <seongyon98@gmail.com>
2024-10-28 08:53:09 +0900

    Delete kr quiz directory

--- kr quiz/program/quiz_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuizAppConfig(AppConfig):
-    name = "quiz_app"
--- kr quiz/program/quiz_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("quiz/", views.quiz, name="quiz"),
-    path("problem/", views.problem, name="problem"),
-    path("results/", views.results, name="results"),
-]
